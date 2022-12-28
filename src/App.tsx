f3979c1cd7f1272739ccf016a35cbb361a8d7197
Marat Mutalipov <maratios41@gmail.com>
2022-12-28 17:05:25 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react';
+import Column from './components/Column';
+import { ColumnType } from './utils/enums';
+
+function App() {
+  return (
+    <>
+      <Heading
+        fontSize={{ base: '4xl', sm: '5xl', md: '6xl' }}
+        fontWeight="bold"
+        textAlign="center"
+        bgGradient="linear(to-l, #7928CA, #FF0080)"
+        bgClip="text"
+        mt={4}>
+        Welcome to Tasks board
+      </Heading>
+      <Container maxWidth="container.lg" px={4} py={10}>
+        <SimpleGrid columns={{base: 1, md: 4}} spacing={{base: 16, md: 4}}>
+          <Column column={ColumnType.TO_DO} />
+          <Column column={ColumnType.IN_PROGRESS} />
+          <Column column={ColumnType.BLOCKED} />
+          <Column column={ColumnType.COMPLETED} />
+        </SimpleGrid>
+      </Container>
+    </>
+  );
+}
+
+export default App;
